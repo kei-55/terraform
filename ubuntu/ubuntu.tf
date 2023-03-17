b039bec9a7c3afff5b111462ce231a6c93289f28
kei__55 <kei@kei--55.com>
2023-03-18 00:34:59 +0900

    説明文の追加 vmidの追加

--- ubuntu/ubuntu.tf
+++ ubuntu/ubuntu.tf
@@ -1,11 +1,18 @@
 resorce "proxmox_instanse" "ubuntu" {
+
   name = "ubuntu"
+  #node名
   node = "pve"
+  #vmid
+  vmid = 100 ~ 200
+  #OSの種類
   os_type = "ubuntu"
+  #cpuのコア数
   cores = 2
+  #メモリのサイズ
   memory = 2048
   disk {
-    size = 20
+    size = 50
     storage = "local"
   }
   network {
